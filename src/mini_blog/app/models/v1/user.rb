9591844e0888e6631497b207ae4632ac11460351
Anh <hoangvietanh291091@gmail.com>
2015-01-22 16:28:41 +0700

    Add comments to controller & model User

--- src/mini_blog/app/models/v1/user.rb
+++ src/mini_blog/app/models/v1/user.rb
@@ -1,57 +1,77 @@
 require 'date'
 module V1
-	class User < ActiveRecord::Base
-		# attr_accessor :password
-		before_save :encrypt_password
+  class User < ActiveRecord::Base
+    # attr_accessor :password
+    #Encrypt password(using salt & hash) before info of user is saved
+    before_save :encrypt_password
 
-		validates :username, :password, :password_confirmation,
-		:firstname, :lastname, :gender, :email, presence: true
+    #validate values not allow null
+    validates :username, :password, :password_confirmation,
+    :firstname, :lastname, :gender, :email, presence: true
 
-		validates :username, length: {minimum: 3}
-		validates :password, length: {minimum: 6}
+    #validate min & max length of values
+    validates :username, length: {minimum: 3}
+    validates :password, length: {minimum: 6}
 
-		validates :password, confirmation: true
+    #validate retype password match password
+    validates :password, confirmation: true
 
-		validates :username, :email, uniqueness: true
+    #validate only exist one value
+    validates :username, :email, uniqueness: true
 
-		validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+    #validate format of email
+    validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
-		validate :check_birthday_valid
+    #validate format of birthday
+    validate :check_birthday_valid
 
-		def self.insert_user(data)
-			user = self.new(data)
-			if((user.save rescue nil))
-				self.return_result({code: 200, description:"Account is created successfully",
-					messages:"Successful",data: nil})
-			else
-				self.return_result({code: 1001, description:"Account is created unsuccessfully",
-					messages:user.errors,data:nil})
-			end
-		end
+    #function to insert user info into DB
+    def self.insert_user(data)
+      user = self.new(data)
+      #check saving user is successfull(fail=>null)
+      if((user.save rescue nil))
+        data = {
+          id: user.id,
+          username: user.username,
+          firstname: user.firstname,
+          lastname: user.lastname
+        }
+        self.return_result({code: 200, description:"Account is created successfully",
+          messages:"Successful",data: data})
+      else
+        self.return_result({code: 1001, description:"Account is created unsuccessfully",
+          messages:user.errors,data:nil})
+      end
+    end
 
-		private
-		def check_birthday_valid
-			if((Date.parse(birthday.to_s) rescue ArgumentError) == ArgumentError)
-				errors.add(:birthday, 'must be a valid day')
-			end
-		end
+    private
+    #function to check birthday is day type(using for validate) 
+    def check_birthday_valid
+      if((Date.parse(birthday.to_s) rescue ArgumentError) == ArgumentError)
+        errors.add(:birthday, 'must be a valid day')
+      end
+    end
 
-		def self.return_result(options)
-			return {
-				meta:{
-					code: options[:code],
-					description: options[:description],
-					messages: options[:messages]
-					},
-					data: options[:data]
-			}
-		end
+    #function to return hash result
+    def self.return_result(options)
+      return {
+        meta:{
+          code: options[:code],
+          description: options[:description],
+          messages: options[:messages]
+          },
+          data: options[:data]
+      }
+    end
 
-		def encrypt_password
-			if password.present?
-				self.salt_password = BCrypt::Engine.generate_salt
-				self.password = BCrypt::Engine.hash_secret(password,salt_password)
-			end
-		end
-	end
+    #fucntion to encrypt password before saving
+    def encrypt_password
+      if password.present?
+        #create salt password
+        self.salt_password = BCrypt::Engine.generate_salt
+        #create hash password
+        self.password = BCrypt::Engine.hash_secret(password,salt_password)
+      end
+    end
+  end
 end
--- src/mini_blog/config/application.rb
+++ src/mini_blog/config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module MiniBlog
   class Application < Rails::Application
     # config.i18n.load_paths += Dir["#{RAILS_ROOT}/app/models/[a-z]*"]
-    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**/}')]
+    # config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**/}')]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
