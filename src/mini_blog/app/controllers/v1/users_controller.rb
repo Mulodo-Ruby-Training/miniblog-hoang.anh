94d6973d5f07091ee6184448378a0a4180fb0238
Anh <hoangvietanh291091@gmail.com>
2015-01-26 14:55:39 +0700

    code update user info

--- src/mini_blog/app/controllers/v1/users_controller.rb
+++ src/mini_blog/app/controllers/v1/users_controller.rb
@@ -24,6 +24,8 @@ module V1
     def login
       username = params[:username]
       password = params[:password]
+
+      #Call function check_login from model V1::User
       if V1::User.check_login(session[:id],session[:token]) == false
         result = V1::User.login(username,password)
         session[:id] = (result[:data][:id]) rescue nil
@@ -31,14 +33,44 @@ module V1
         render json: result
       else
         render json:{
-        meta:{
-          code: 2001,
-          description: "This username is already in used",
-          messages: "Already login"
+          meta:{
+            code: 2001,
+            description: "This username is already in used",
+            messages: "Already login"
           },
           data: nil
         }
       end
     end
+
+    def update
+      data_input = {
+        firstname: params[:firstname],
+        lastname: params[:lastname],
+        avatar: params[:avatar],
+        address: params[:address],
+        city: params[:city],
+        email: params[:email],
+        mobile: params[:mobile],
+        gender: params[:gender],
+        birthday: params[:birthday]
+      }
+
+      #Call function check_login from model V1::User
+      if V1::User.check_login(session[:id],session[:token])
+        #Call function update_user from model V1:User and render result to json
+        render json: V1::User.update_user(params[:id],data_input)
+      else
+        render json:{
+          meta:{
+            code: 1002,
+            description: "Token invalid",
+            messages: "Unsuccessful"
+            },
+          data: nil
+        }
+      end
+    end
+
   end
 end
