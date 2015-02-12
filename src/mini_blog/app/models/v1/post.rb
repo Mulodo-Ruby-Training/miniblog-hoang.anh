092a6b3eb5066d515057a3202605ce8fba8a2fc3
Anh <hoangvietanh291091@gmail.com>
2015-02-12 13:11:14 +0700

    Add constants for error code & error message

--- src/mini_blog/app/models/v1/post.rb
+++ src/mini_blog/app/models/v1/post.rb
@@ -13,10 +13,10 @@ module V1
     def self.insert_post(data)
       post = self.new(data)
       if(post.save rescue nil)
-        V1::User.return_result({code: 200, description:"Post is created successfully",
+        V1::User.return_result({code: STATUS_OK, description:"Post is created successfully",
             messages:"Successful",data: nil})
       else
-        V1::User.return_result({code: 1001, description:"Post is created unsuccessfully",
+        V1::User.return_result({code: ERROR_VALIDATE, description:MSG_VALIDATE,
             messages:post.errors,data: nil})
       end
     end
@@ -25,10 +25,10 @@ module V1
     def self.update_post(post_id,data)
       post = self.find(post_id)
       if(post.update(data) rescue nil)
-        V1::User.return_result({code: 200, description:"Post is updated successfully",
+        V1::User.return_result({code: STATUS_OK, description:"Post is updated successfully",
             messages:"Successful",data: nil})
       else
-        V1::User.return_result({code: 1001, description:"Post is updated unsuccessfully",
+        V1::User.return_result({code: ERROR_VALIDATE, description:MSG_VALIDATE,
             messages:post.errors,data: nil})
       end
     end
@@ -37,25 +37,25 @@ module V1
     def self.delete_post(post_id)
       post = self.find(post_id)
       if(post.destroy() rescue nil)
-        V1::User.return_result({code: 200, description:"Post is deleted successfully",
+        V1::User.return_result({code: STATUS_OK, description:"Post is deleted successfully",
               messages:"Successful",data: nil})
       else
-        V1::User.return_result({code: 2504, description:"Post is deleted unsuccessfully",
+        V1::User.return_result({code: ERROR_DELETE_POST_FAILED, description:MSG_DELETE_POST_FAILED,
               messages:"Unsuccessful",data: nil})
       end
     end
 
     def self.active_or_deactive_post(post_id,status)
       if post_id == "" || status == ""
-        V1::User.return_result({code: 1001, description:"Post is updated unsuccessfully",
+        V1::User.return_result({code: ERROR_VALIDATE, description:MSG_VALIDATE,
               messages:"Post id or status not input",data: nil})
       end
       post = self.find(post_id)
       if(post.update_attribute('status', status) rescue nil)
-        V1::User.return_result({code: 200, description:"Post is updated successfully",
+        V1::User.return_result({code: STATUS_OK, description:"Post is updated successfully",
               messages:"Successful",data: nil})
       else
-        V1::User.return_result({code: 2503, description:"Post is updated unsuccessfully",
+        V1::User.return_result({code: ERROR_UPDATE_POST_FAILED, description:MSG_UPDATE_POST_FAILED,
               messages:"Unsuccessful",data: nil})
       end
     end
