2843660a0838878700e900a3f87592db72cb8ca3
Anh <hoang.anh@mulodo.com>
2015-04-01 13:05:17 +0700

    Checking param birthday

--- src/mini_blog_client/app/controllers/users_controller.rb
+++ src/mini_blog_client/app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   def create
-    params[:user][:birthday] = Date.civil(params[:user]["birthday(1i)"].to_i,params[:user]["birthday(2i)"].to_i,params[:user]["birthday(3i)"].to_i)
+    if params[:user]["birthday(1i)"] !="" && params[:user]["birthday(2i)"] != "" && params[:user]["birthday(3i)"] != ""
+      params[:user][:birthday] = Date.civil(params[:user]["birthday(1i)"].to_i,params[:user]["birthday(2i)"].to_i,params[:user]["birthday(3i)"].to_i)
+    end
+    # if params[:user][:avatar]
+    #   data_image = StringIO.new(Base64.decode64(params[:user][:avatar]))
+    #   data_image.class.class_eval { attr_accessor :original_filename, :content_type }
+    #   data_image.original_filename = params[:user][:avatar].original_filename
+    #   data_image.content_type = params[:user][:avatar].content_type 
+    #   params[:user][:avatar] = data_image
+    # end
     data_input = params[:user]
     resp = Net::HTTP.post_form(URI.parse('http://localhost:3000/v1/users'),data_input)
     data_output = JSON.parse(resp.body)
@@ -9,8 +18,7 @@ class UsersController < ApplicationController
     else
       flash[:notice] = data_output["meta"]["description"]
     end
-    
-    # flash[:notice] = data_input
+
     redirect_to({action:'signup'})
   end
 end
